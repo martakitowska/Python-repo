44908ddf1d78434269817ccb304d2a12d5b06a6b
Kitowska, Marta <marta.kitowska@intel.com>
2025-11-19 11:24:27 +0100

    Resolution for 6 homework task added.

---
+++ wybor_programu.py
@@ -0,0 +1,12 @@
+import silnia, piramida, radius, box_drawing, rok_przestepny, rozmienianie_monet, dziesietna, kalkulator_temperatur
+
+print("Zadania: "
+      "1. kalkulator_temperatur/n"
+      "2. silnia/n"
+      "3. piramida/n"
+      "4. radius/n"
+      "5. box_drawing/n"
+      "6. rok_przestepny/n"
+      "7. rozmienianie_monet/n"
+      "8. dziesietna/n")
+
